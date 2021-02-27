be27daaaac9bde42568cce4af653711d770c88a3
matsujun <matsujun365@gmail.com>
2021-02-27 13:36:22 +0900

    format

--- app/src/main/java/dev/matsujun/puppies/data/FakeData.kt
+++ app/src/main/java/dev/matsujun/puppies/data/FakeData.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.matsujun.puppies.data
 
 import java.time.LocalDate
@@ -7,7 +22,7 @@ val puppies = listOf(
         id = 0,
         name = "Max",
         imageUrl = "https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2020,1,7),
+        birthDay = LocalDate.of(2020, 1, 7),
         ownerName = "Michael"
     ),
 
@@ -15,7 +30,7 @@ val puppies = listOf(
         id = 1,
         name = "Charlie",
         imageUrl = "https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2019,3,27),
+        birthDay = LocalDate.of(2019, 3, 27),
         ownerName = "Stephanie"
     ),
 
@@ -23,28 +38,28 @@ val puppies = listOf(
         id = 2,
         name = "Buddy",
         imageUrl = "https://images.pexels.com/photos/57627/pexels-photo-57627.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2010,7,15),
+        birthDay = LocalDate.of(2010, 7, 15),
         ownerName = "James"
     ),
     Puppy(
         id = 3,
         name = "Cooper",
         imageUrl = "https://images.pexels.com/photos/129634/pexels-photo-129634.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2021,9,6),
+        birthDay = LocalDate.of(2021, 9, 6),
         ownerName = "Christopher"
     ),
     Puppy(
         id = 4,
         name = "Jack",
         imageUrl = "https://images.pexels.com/photos/3726314/pexels-photo-3726314.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2015,8,21),
+        birthDay = LocalDate.of(2015, 8, 21),
         ownerName = "Nicole"
     ),
     Puppy(
         id = 5,
         name = "Rocky",
         imageUrl = "https://images.pexels.com/photos/4490129/pexels-photo-4490129.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2017,5,16),
+        birthDay = LocalDate.of(2017, 5, 16),
         ownerName = "Robert"
     ),
 
@@ -52,7 +67,7 @@ val puppies = listOf(
         id = 6,
         name = "Bear",
         imageUrl = "https://images.pexels.com/photos/4587998/pexels-photo-4587998.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2015,3,12),
+        birthDay = LocalDate.of(2015, 3, 12),
         ownerName = "Heather"
     ),
 
@@ -60,91 +75,91 @@ val puppies = listOf(
         id = 7,
         name = "Duke",
         imageUrl = "https://images.pexels.com/photos/2523934/pexels-photo-2523934.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2014,4,10),
+        birthDay = LocalDate.of(2014, 4, 10),
         ownerName = "Jessica"
     ),
     Puppy(
         id = 8,
         name = "Toby",
         imageUrl = "https://images.pexels.com/photos/3478875/pexels-photo-3478875.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2019,1,3),
+        birthDay = LocalDate.of(2019, 1, 3),
         ownerName = "Joseph"
     ),
     Puppy(
         id = 9,
         name = "Tucker",
         imageUrl = "https://images.pexels.com/photos/2145878/pexels-photo-2145878.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2018,2,18),
+        birthDay = LocalDate.of(2018, 2, 18),
         ownerName = "Elizabeth"
     ),
     Puppy(
         id = 10,
         name = "Bella",
         imageUrl = "https://images.pexels.com/photos/6291572/pexels-photo-6291572.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2020,11,22),
+        birthDay = LocalDate.of(2020, 11, 22),
         ownerName = "David"
     ),
     Puppy(
         id = 11,
         name = "Lucy",
         imageUrl = "https://images.pexels.com/photos/1390361/pexels-photo-1390361.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2016,10,26),
+        birthDay = LocalDate.of(2016, 10, 26),
         ownerName = "John"
     ),
     Puppy(
         id = 12,
         name = "Daisy",
         imageUrl = "https://images.pexels.com/photos/2664417/pexels-photo-2664417.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2013,8,30),
+        birthDay = LocalDate.of(2013, 8, 30),
         ownerName = "Sarah"
     ),
     Puppy(
         id = 13,
         name = "Lola",
         imageUrl = "https://images.pexels.com/photos/1420405/pexels-photo-1420405.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2020,1,1),
+        birthDay = LocalDate.of(2020, 1, 1),
         ownerName = "Daniel"
     ),
     Puppy(
         id = 14,
         name = "Luna",
         imageUrl = "https://images.pexels.com/photos/1619690/pexels-photo-1619690.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2017,2,4),
+        birthDay = LocalDate.of(2017, 2, 4),
         ownerName = "Ashley"
     ),
     Puppy(
         id = 15,
         name = "Molly",
-        imageUrl= "https://images.pexels.com/photos/672244/pexels-photo-672244.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2015,3,14),
+        imageUrl = "https://images.pexels.com/photos/672244/pexels-photo-672244.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        birthDay = LocalDate.of(2015, 3, 14),
         ownerName = "Amanda"
     ),
     Puppy(
         id = 16,
         name = "Sadie",
         imageUrl = "https://images.pexels.com/photos/257519/pexels-photo-257519.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2017,5,7),
+        birthDay = LocalDate.of(2017, 5, 7),
         ownerName = "Matthew"
     ),
     Puppy(
         id = 17,
         name = "Sophie",
         imageUrl = "https://images.pexels.com/photos/485294/pexels-photo-485294.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2014,2,8),
+        birthDay = LocalDate.of(2014, 2, 8),
         ownerName = "Megan"
     ),
     Puppy(
         id = 18,
         name = "Bailey",
         imageUrl = "https://images.pexels.com/photos/373467/pexels-photo-373467.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        birthDay = LocalDate.of(2019,9,11),
+        birthDay = LocalDate.of(2019, 9, 11),
         ownerName = "Jennifer"
     ),
     Puppy(
         id = 19,
         name = "Maggie",
         imageUrl = "https://images.pexels.com/photos/1307630/pexels-photo-1307630.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-        birthDay = LocalDate.of(2020,6,22),
+        birthDay = LocalDate.of(2020, 6, 22),
         ownerName = "Joshua"
     ),
 )
